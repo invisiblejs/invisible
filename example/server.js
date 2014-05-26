fab285ff450911ff05e5fb8e64419819b41e5c7f
Martin Paulucci <martin.c.paulucci@gmail.com>
2014-05-26 12:14:55 +0200

    Better example app.

--- example/server.js
+++ example/server.js
@@ -1,9 +1,19 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
 var invisible = require('../');
 var bodyParser = require('body-parser');
 
-
 var app = express();
 app.use(bodyParser());
-invisible.createServer(app, path.join(__dirname, 'models'));
+
+app.use(invisible.router({
+  rootFolder: path.join(__dirname, 'models')
+}));
+
+app.get('/', function(req, res) {
+  res.sendfile('index.html');
+});
+
+app.listen(3000);
